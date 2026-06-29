e1d9ba399278911a8a24878a8fd6f427350b963b
OzNice1too <oz.schoolmail@gmail.com>
2026-06-29 12:21:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function evenCheck(){
+    let userInput=prompt("Pick a number");
+    x=Math.sqrt()
+    if((userInput*userInput)%2==0){
+        document.writeln("<div class=header>"+"the power of 2 of your number is even"+"</div>")    
+    }
+    else{
+        document.writeln("<div class=header>"+"the power of 2 of your number is odd"+"</div>")            
+    }
+}
+evenCheck()
